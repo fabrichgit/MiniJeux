3b0880a623dada92727ae648c670bac000489c27
fabrich <fabrichjean@gmail.com>
2023-05-02 08:08:58 +0100

    miniJeux

---
+++ apk.js
@@ -0,0 +1,629 @@
+let allword = ["TANYA","SCHUYLER","CARY",
+"RAMONA","GEORGE","NATHANIEL",
+"DORA","HILBERT","PANSY",
+"TOBIN","GEORGE","ROSALYN",
+"ELDON","TRACEY","LAYNE",
+"ODA","HAL","KAMERON",
+"CIARA","URIAH","EVIE",
+"AKEEM","FREDERIQUE","GABRIELLE",
+"MILLER","CAMERON","JOVAN",
+"HERMANN","KENDALL","DESTINI",
+"JACQUELYN","MAGDALENA","WAYNE",
+"JULIA","HARMON","ANNAMARIE",
+"DELFINA","JAY","BARON",
+"JERALD","JUDGE","NICHOLAUS",
+"HILMA","GARFIELD","ALESSANDRO",
+"DUDLEY","MYRTICE","JACYNTHE",
+"DEVIN","WILFRID","JESSY",
+"GARETT","LOLITA","WOODROW",
+"BRIANNE","CRYSTEL","JOVAN",
+"EMMITT","DEJAH","KELVIN",
+"SABRINA","MAZIE","MABEL",
+"HOLLIS","STELLA","ARMANDO",
+"TYRA","STANTON","EMELY",
+"ZOE","SYLVIA","KEVON",
+"ELSIE","CIELO","ROCIO",
+"NED","LYNN","OLETA",
+"ABEL","MACK","RODOLFO",
+"CARTER","ENA","HEATHER",
+"SAMMIE","JANICK","PRISCILLA",
+"LUZ","FLETA","THEO",
+"JULIE","ANIYAH","VERNICE",
+"ADITYA","SAMIR","BROOKS",
+"TYSON","ALANA","ROWAN",
+"JASON","OMER","EVELYN",
+"TITO","ENRIQUE","JANIYA",
+"EMMANUELLE","HOBART","RYDER",
+"NASIR","JAMISON","DORCAS",
+"BREANA","CLARE","TAMARA",
+"LUCINDA","KARLEY","NOAH",
+"JOVANY","HELOISE","JERROLD",
+"ERICK","JUANA","ERICK",
+"SIDNEY","WENDELL","LAYLA",
+"KALLIE","TRACY","NIKKI",
+"ZACKARY","LONDON","ERIK",
+"KAELYN","JOHNATHON","FLAVIO",
+"ANTONIO","JACKY","LORNA",
+"ANNETTE","CRYSTAL","STEPHEN",
+"MAURICIO","KYLA","ASHLEIGH",
+"NYAH","JENNYFER","YVONNE",
+"KELTON","RICO","CARMEN",
+"EDMOND","MARGARETE","KESHAUN",
+"DELPHA","MANLEY","ALYCIA",
+"ANSLEY","JOEL","TREVION",
+"CARISSA","DELANEY","RUSS",
+"SONYA","CARISSA","MERVIN",
+"MARTINA","HELGA","JUDAH",
+"KALI","DWIGHT","EVA",
+"KACEY","HOLDEN","LEOPOLDO",
+"JODY","NESTOR","BRANDYN",
+"KENDRICK","EBONY","JOSEFINA",
+"CAMILLE","ABIGALE","MILFORD",
+"CARLIE","ALICIA","LOTTIE",
+"MAYRA","DANNIE","JOSEFINA",
+"LAURA","BETTY","MARCUS",
+"PERRY","COTY","FELTON",
+"ARNE","MARIANO","KAYCEE",
+"MACI","KAMERON","CHASE",
+"SAVANNAH","GARRET","MARKUS",
+"ROZELLA","STELLA","PRUDENCE",
+"UNIQUE","KARIANNE","ALEXANDRE",
+"ADOLF","JULIANNE","ANYA",
+"KASSANDRA","NIKO","ADALINE",
+"JAZMYNE","CHARLES","ADELL",
+"LORENZA","DAMARIS","CLAY",
+"JARRELL","ALEXANDRE","JOHNNIE",
+"JARED","BARON","REAGAN",
+"NEIL","JAMEY","STEVE",
+"OSWALD","ARMAND","SAUL",
+"NYAH","CYRIL","DANIKA",
+"WENDY","ROBYN","ASIA",
+"DERON","LEONORA","SEBASTIAN",
+"EVALYN","WAVA","JEWEL",
+"DOMINIC","ALEXANDREA","DASIA",
+"FRANKIE","BRADLEY","ROBERTA",
+"NORENE","FREDERIQUE","JACLYN",
+"JUSTINA","ROBERTA","ZAKARY",
+"ANNABELLE","GERARD","JOHNATHAN",
+"LLEWELLYN","ARVILLA","NICOLETTE",
+"JANE","LAVON","AVA",
+"JERAMIE","LEWIS","KEIRA",
+"ALBERTA","JADA","DAYNE",
+"KIERA","AUDIE","URIAH",
+"DAYNE","JUANITA","GREGORIO",
+"TILLMAN","BROWN","COLLIN",
+"ANA","HEAVEN","EULALIA",
+"KAILEY","VINCE","EMMETT",
+"EMMALEE","KACEY","TIANA",
+"DANE","CHANEL","KIARRA",
+"NASH","BROOKE","ANNAMARIE",
+"NEIL","CARMELO","RHIANNA",
+"IBRAHIM","CORALIE","HALEIGH",
+"JEANIE","RAMON","HOWELL",
+"BRANDON","RITA","FREDDIE",
+"KENNA","SOLEDAD","ROY",
+"GERALD","AVA","TIARA",
+"TRESSA","DAWSON","DEVAN",
+"ASHLY","JACINTHE","LEATHA",
+"ANGUS","TROY","NOEL",
+"BERNEICE","NAT","MAGDALEN",
+"ALVIS","CALEB","NIKKI",
+"JUANA","FAYE","DECLAN",
+"CARLEE","ESTEFANIA","MATTIE",
+"KELLEN","CELINE","MICHALE",
+"NATASHA","CALI","DESSIE",
+"DEVEN","ISAIAS","KASSANDRA",
+"KESHAUN","GREGG","WYATT",
+"TRESSA","KODY","ETHYL",
+"MARLIN","JETT","ANAIS",
+"LIBBY","DUSTIN","BELLA",
+"AMERICA","GARLAND","JOELLE",
+"ISAC","JOSEFA","SONIA",
+"RODOLFO","HALEIGH","LORAINE",
+"KRISTINA","MAXIMUS","ELIZABETH",
+"JAMIL","FREIDA","WALTON",
+"LAURYN","BONITA","LOUVENIA",
+"ALTHEA","CRISTOBAL","MARLENE",
+"HOLDEN","PRECIOUS","STEWART",
+"BESSIE","HENRY","JAQUAN",
+"ROSSIE","CANDICE","ALF",
+"ALI","GARRICK","GEOVANNY",
+"MARJOLAINE","LAZARO","RICKIE",
+"JEFFERY","ESTEL","KELLEY",
+"EPHRAIM","RETHA","DONALD",
+"DANYKA","ELNA","JAMAAL",
+"KEON","HAZEL","SASHA",
+"BRIDGETTE","DREW","HARRY",
+"PERRY","RICKIE","KIAN",
+"DARION","JALEN","RILEY",
+"ROBERTO","SASHA","SAVANNAH",
+"CORTEZ","AMOS","ALFORD",
+"AMANI","KATLYNN","IDA",
+"SHAWNA","SANDRINE","CHARLOTTE",
+"LARON","RON","JOHNATHAN",
+"WINNIFRED","ANIYAH","LEON",
+"CADE","KACIE","FAUSTINO",
+"DARION","CAESAR","JAMIR",
+"BRADY","CEASAR","VERDIE",
+"HOLLIS","LAVINA","LEONIE",
+"EDISON","REYNA","LILLIE",
+"EFFIE","JANE","ALBERTHA",
+"JACINTHE","CHEYENNE","VICKIE",
+"JULES","BRIDGET","JEREMY",
+"JULIE","NOEMY","CARMEL",
+"TREMAINE","KENDRICK","DAMON",
+"RAE","JAMIR","GABRIELLE",
+"BRANDI","ENA","TERRY",
+"BROOKLYN","LILYAN","ANABELLE",
+"SUSANNA","MERTIE","CHRISTIAN",
+"EBBA","ROSALEE","VLADIMIR",
+"ANGUS","BRIGITTE","ISOM",
+"EDISON","NEVA","KELLY",
+"FELIPE","JORDY","NICOLE",
+"DEE","HENDERSON","JAYNE",
+"AISHA","LORA","LURA",
+"JUDGE","JOYCE","KASANDRA",
+"ALYSHA","SCARLETT","CRISTINA",
+"CAREY","ETHA","WESTON",
+"SADIE","JULIEN","ANASTASIA",
+"ERNESTINA","MERTIE","BROCK",
+"CASIMER","SOPHIE","KIRK",
+"LLEWELLYN","JAZLYN","EASTON",
+"TRENT","ELFRIEDA","ISABELLA",
+"WAYNE","BERENICE","GILDA",
+"ROWAN","JACQUES","GRIFFIN",
+"TAUREAN","DESTINEE","CARMELO",
+"OPHELIA","LUCY","DANA",
+"KIM","MAXWELL","ISAAC",
+"CARRIE","ANGIE","DORIS",
+"EFRAIN","SHEMAR","TIA",
+"DAMEON","SERENA","LAVINIA",
+"NARCISO","KEYSHAWN","BENJAMIN",
+"LINDA","MEKHI","DARRICK",
+"AIDAN","HOUSTON","KATLYN",
+"ENRIQUE","ELEANORA","ALEXANDRO",
+"ROMAINE","TEAGAN","DEANGELO",
+"ALBERTHA","JETTIE","EDMOND",
+"EMERALD","ROGER","EDYTHE",
+"COOPER","PAUL","HARLEY",
+"BULAH","SEAMUS","NONA",
+"BLANCA","CLEMENS","JOY",
+"MOZELL","JANIS","MARCUS",
+"ANYA","COY","SABRYNA",
+"SANTIAGO","ALBERTA","VINCENZO",
+"RUBY","MAIA","ANDREANNE",
+"ARDELLA","RICHIE","SHANNA",
+"MODESTO","LIBBY","EVANS",
+"CREOLA","DAVONTE","ARIANE",
+"SIDNEY","FRANCIS","CONNER",
+"ALBERTO","CHRISTY","GENESIS",
+"ELSE","MAYA","JUSTINA",
+"PETRA","VITA","ARIELLE",
+"TERRELL","LINA","KRISTA",
+"MANUELA","CITLALLI","FRANCISCO",
+"STEPHEN","THALIA","EVANGELINE",
+"EVANGELINE","VELMA","ISAIAS",
+"NAPOLEON","MINERVA","ELISHA",
+"JOAN","SHAKIRA","WILLOW",
+"CLEMENS","ELWIN","DALTON",
+"DUNCAN","HALEIGH","ANDRES",
+"SADYE","JORDON","RONALDO",
+"ED","PRECIOUS","DWIGHT",
+"MOLLIE","WANDA","JANA",
+"NICKOLAS","GILBERTO","SHANE",
+"ORAL","FREDERIQUE","ALTHEA",
+"SYBLE","SARINA","PEDRO",
+"FRIEDA","EZRA","ADRIEL",
+"ODA","ELYSE","PEGGIE",
+"DURWARD","GEOVANNY","WESTON",
+"EBONY","ASTRID","SIENNA",
+"CIELO","ITZEL","BESSIE",
+"KENYA","GEORGIANA","MCKENZIE",
+"UBALDO","LONDON","ISAAC",
+"KOLBY","ARVID","ABIGAYLE",
+"JULIE","ELLIOTT","AUDIE",
+"MAYBELLE","ARVEL","CARLETON",
+"MAYNARD","DARYL","EMELIA",
+"HIRAM","ARIELLE","GEORGE",
+"DIEGO","JEANNE","RILEY",
+"KAYLIN","SHEILA","CAMERON",
+"ELZA","NOLA","LINNIE",
+"ELDRIDGE","DOLORES","DELANEY",
+"GUNNER","LUCIUS","BABY",
+"CARLOS","LAURYN","OSWALDO",
+"MAYBELL","LORENZ","KELVIN",
+"KOLE","JONAS","CHAZ",
+"PAYTON","JANA","JACKLYN",
+"MORTON","BERNICE","MINERVA",
+"ANGELITA","DOMINGO","MYRIAM",
+"JULIEN","KAYLI","GERMAN",
+"SUSIE","NYA","RANDI",
+"TYLER","MATHIAS","JERRELL",
+"CLYDE","ALEXANDRA","DAMON",
+"DERECK","ETHELYN","KATRINA",
+"MOHAMED","EDWARD","YOLANDA",
+"KING","ANIYAH","HAVEN",
+"AIMEE","PROVIDENCI","NORWOOD",
+"SOPHIA","MERLE","DARIANA",
+"ALDEN","BRENNA","CARRIE",
+"CASSIDY","GARFIELD","THAD",
+"ELWYN","COLEMAN","ORLANDO",
+"VESTA","DUNCAN","CRISTIAN",
+"NETTIE","JAQUELIN","MARCO",
+"AGUSTINA","MARYAM","LILIANE",
+"MARCO","CORTEZ","EMERSON",
+"FREDDIE","DOUGLAS","DENA",
+"SAGE","JACINTHE","TYRESE",
+"CLEORA","MILFORD","PEARLIE",
+"ABE","EMMY","CYNTHIA",
+"FINN","BEATRICE","HALEY",
+"CORY","ANGELO","OKEY",
+"ABBEY","OSBALDO","IDA",
+"KRISTOPHER","ARMANI","GAETANO",
+"LEXUS","ESTELLA","JERMAIN",
+"MELYSSA","JILLIAN","CRISTAL",
+"LELAH","ALVA","HANNA",
+"ELISABETH","LANCE","SCARLETT",
+"ARNE","BERNEICE","NANNIE",
+"IMOGENE","GLADYS","MERVIN",
+"KATRINA","LOU","CHASE",
+"FAE","NED","DUSTIN",
+"FRIEDA","LORINE","JAMEL",
+"LLEWELLYN","CONNER","GRANT",
+"MARGARETTA","ELISEO","KYLEE",
+"MARIELLE","MACI","DEDRICK",
+"MAYNARD","REUBEN","HERTA",
+"LUCAS","GARRISON","JAQUAN",
+"MILLER","RAFAEL","NORVAL",
+"ELZA","JAMIL","DINO",
+"DEVIN","REVA","MARGARITA",
+"RAUL","CIARA","KAYCEE",
+"MARLENE","TERRILL","KEVIN",
+"DILLAN","STACEY","SAMMIE",
+"SKYLAR","MAX","LUPE",
+"MARCIA","CORENE","ALIA",
+"BLAKE","BRIELLE","MARYAM",
+"JUDD","LELAH","DEMARIO",
+"ARACELY","EMMETT","HILBERT",
+"ALLAN","TARYN","DAMIAN",
+"ALBIN","LACEY","MELYNA",
+"MERTIE","KARI","SHANNON",
+"DAYNA","HALEY","TATYANA",
+"LYDA","GERALDINE","ANIYA",
+"CONNIE","TERESA","NADIA",
+"CORALIE","PATTIE","JOSIE",
+"MARKUS","ANAIS","DARYL",
+"DOMENICK","ISOM","JENNIFER",
+"MEAGHAN","KARLEY","SHIRLEY",
+"COURTNEY","BUSTER","DAWN",
+"KENNA","WILHELM","MATEO",
+"SHANIYA","CRISTOBAL","ELENA",
+"NILS","MCKENZIE","GUS",
+"FELIPA","JAQUELIN","SAGE",
+"ALEC","CORDELL","IDELLA",
+"SUSIE","BAILEY","SCHUYLER",
+"VICKY","DANDRE","LORINE",
+"COTY","VELDA","LINDSEY",
+"OLGA","SHANON","VICENTA",
+"BRADY","DANNIE","AMYA",
+"MUSTAFA","MIKAYLA","KATHERINE",
+"JORDAN","BRAXTON","FERNANDO",
+"LEONARD","ORIE","NOVELLA",
+"ALEXANDER","MINA","FOSTER",
+"KAYLIN","JEROD","SERENITY",
+"POLLY","MARYAM","KARINE",
+"DEMETRIUS","MAGDALENA","JOANNIE",
+"NATHAN","ESTEFANIA","TIANA",
+"CLINT","JADA","FRIEDRICH",
+"MARILOU","ROSCOE","ALFREDO",
+"TREVER","ENRIQUE","QUINCY",
+"DONNIE","DONNELL","EFFIE",
+"GARLAND","ROSELYN","LUTHER",
+"HILLARY","DIANA","ISABEL",
+"NEAL","MIGUEL","CHASITY",
+"FRANCESCO","ROSINA","DORIAN",
+"ALEXIS","BARBARA","JOANNE",
+"FAY","MILES","WINIFRED",
+"MYRTICE","BEN","FLORENCIO",
+"ROSENDO","BRIDGETTE","LORENZA",
+"VEDA","HALLE","WADE",
+"BENTON","ZELDA","KORBIN",
+"JAYLEN","REGGIE","JORGE",
+"JEREL","BARON","THEO",
+"LELIA","GENEVIEVE","MYRTLE",
+"KITTY"];
+const alph= ['A','B','C','D','E',
+   'F','G','H','I','J','K','L',
+   'M','N','O','P','Q','R','S',
+   'T','U','V','W','X','Y','Z'
+];
+
+let eachLetter= document.querySelectorAll(".content-word div");
+let car= document.getElementById('car');
+let word='';
+let win=false;
+
+
+function getRadomElement(array) {
+    let randomIndex = Math.floor(Math.random()*1000);
+    return array[randomIndex];
+}
+
+function removePX(value) {
+    let result='';
+    let index=0;
+    while (value[index]!='p') {
+        result+=value[index];
+        index++;
+    }
+        return +result;
+}
+
+function showRadomWord() {
+  
+    word= getRadomElement(allword);
+    let content_word= document.querySelector(".content-word")
+
+    for(let index= 0; index<=word.length-1; index++){
+        let wordElement= document.createElement("div");
+        wordElement.setAttribute("id", `letter${index}`);
+        wordElement.setAttribute("class", word[index]);
+        content_word.appendChild(wordElement);
+        wordElement.innerHTML= word[index];
+    }
+
+}
+
+function initWord() {
+
+    word.split('').forEach(element => {
+        document.querySelector(`.${element}`).remove();
+      //  console.log(element);
+    });
+    word='';
+
+    showRadomWord();
+    numberTypingTrue=0;
+    count++;
+
+    if (count==50) {
+        win=true;
+        stop(win);
+        count=0;
+    }
+}
+
+
+function runAll(rank, status) {
+    for(let index=1; index<=4; index++){
+        document.querySelector(`#line1 div:nth-child(${index})`).style.animation=`run 4s -${index-1}s infinite linear`;
+        document.querySelector(` #line2 div:nth-child(${index})`).style.animation=`run 4s -${index-1}s infinite linear`;
+    }
+    document.getElementById('carPNG1').style.animation='run 3s infinite linear';
+    document.getElementById('carPNG2').style.animation='run 3s 1s infinite linear';
+    document.getElementById('carPNG3').style.animation='run 3.5s infinite linear';
+    document.getElementById('carPNG4').style.animation='run 3.5s 1s infinite linear';
+    document.getElementById("car").style.display= "block";
+    document.getElementById("menu").style.display= "none";
+
+
+    document.querySelector(".live").innerHTML=
+    `
+        <div id="mistake1"></div>
+        <div id="mistake2"></div>
+        <div id="mistake3"></div>
+        <div id="mistake4"></div>
+        <div id="mistake5"></div>
+        <div id="mistake6"></div>
+        <div id="mistake7"></div>
+        <div id="mistake8"></div>
+        <div id="mistake9"></div>
+        <div id="mistake10"></div>
+        <div id="mistake11"></div>
+        <div id="mistake12"></div>
+        <div id="mistake13"></div>
+        <div id="mistake14"></div>
+        <div id="mistake15"></div>
+    `
+
+    let second;
+    let minutes;
+    
+    if (status==false) {
+        console.log("nonPers");
+        if (rank=="moyen") {
+            document.getElementById('zero').innerHTML='0';
+            document.getElementById('minute').innerHTML='2';
+            document.getElementById('double').innerHTML=':';
+            document.getElementById('second1').innerHTML='0';
+            document.getElementById('second2').innerHTML='0';   
+    
+            second=59;
+            minutes= 1;
+    
+        }else if (rank=="difficile") {
+            
+            document.getElementById('zero').innerHTML='0';
+            document.getElementById('minute').innerHTML='1';
+            document.getElementById('double').innerHTML=':';
+            document.getElementById('second1').innerHTML='3';
+            document.getElementById('second2').innerHTML='0';   
+    
+            second=29;
+            minutes= 1;
+    
+        }else if (rank=="facile") {
+            
+            document.getElementById('zero').innerHTML='0';
+            document.getElementById('minute').innerHTML='2';
+            document.getElementById('double').innerHTML=':';
+            document.getElementById('second1').innerHTML='3';
+            document.getElementById('second2').innerHTML='0';   
+    
+            second=29;
+            minutes= 2;
+    
+        }
+
+    }else{
+
+        console.log("pers");
+        minutes= rank[0];
+        second= rank[1]-1;
+
+        document.getElementById('zero').innerHTML='0';
+        document.getElementById('minute').innerHTML= minutes;
+        document.getElementById('double').innerHTML=':';
+        console.log(second.toString());
+        if (rank[1].toString().length==1) {
+            document.getElementById('second1').innerHTML='0';
+            document.getElementById('second2').innerHTML= second+1;
+        }else if(rank[1].toString().length==2){
+            console.log("double");
+            document.getElementById('second1').innerHTML= rank[1].toString()[0];
+            document.getElementById('second2').innerHTML= parseInt(second.toString()[1])+1;
+        }
+
+    }
+
+    setInterval(() => {
+        if (second==0 && minutes==0) {
+            document.getElementById('second2').innerHTML= '0';
+            win=false;
+            stop(win);
+        }else{
+
+            if (second==-1) {
+                second = 59;
+                minutes--;
+            }
+            if (second.toString().length==2) {
+                document.getElementById('second1').style.display='none';
+            }else{
+                document.getElementById('second1').style.display='inline';
+                document.getElementById('second1').innerHTML= '0';
+            }
+            document.getElementById('minute').innerHTML= minutes;
+            document.getElementById('second2').innerHTML= second;
+    
+            second--;
+
+        }
+
+
+    }, 1000);
+
+}
+
+let mistakes=0;
+let numberTypingTrue=0;
+function onTyping() {
+    
+    window.addEventListener("keydown", (clicked)=>{
+
+        if (clicked.key==word[numberTypingTrue]) {
+            document.getElementById(`letter${numberTypingTrue}`).style.color='red';
+            numberTypingTrue++;
+    
+            let LeftCar= window.getComputedStyle(car).left;
+            car.style.left= `${removePX(LeftCar)+5}px`;
+    
+            if (window.getComputedStyle(car).left=='1300px') {
+                win=true;
+                stop(win);
+            }else{
+    
+                if (numberTypingTrue==word.length) {
+                    document.querySelector(".wordToTap").classList.add("true");
+                    setTimeout(() => {
+                        document.querySelector(".wordToTap").classList.remove("true");
+                    }, 200);
+                    initWord();
+                }
+            }
+    
+            document.querySelector(".wordToTap").classList.add("true");
+            setTimeout(() => {
+                document.querySelector(".wordToTap").classList.remove("true");
+            }, 200);
+    
+        }else{
+            mistakes++;
+            document.getElementById(`mistake${mistakes}`).style.backgroundColor='black';
+            document.getElementById(`mistake${mistakes}`).style.border=' dotted 1px white';
+            document.querySelector(".wordToTap").classList.add("false");
+            setTimeout(() => {
+                document.querySelector(".wordToTap").classList.remove("false");
+            }, 200);
+            if (mistakes==15) {
+                win= false;
+                stop(win);
+            }
+        }
+    })
+
+}
+
+let persOption= false;
+document.getElementById("perso").addEventListener("click", ()=>{
+    persOption = true;
+    console.log(persOption);
+    document.getElementById("niveau").style.display='none';
+    document.getElementById("optionPerso").innerHTML=`<div>
+    <span style="font-size: medium;">Minute</span>
+    <span>
+        <input id="min" type="number" value="2" min="0" max="4" step="1" style="padding: 0.5vw 0.5vw; font-size: medium;">
+    </span>
+</div> 
+<div>
+    <span style="font-size: medium; font-family: Verdana; font-weight: bold;">Second</span>
+    <span>
+        <input id="sec" type="number" value="0" min="0" max="59" step="1" style="padding: 0.5vw 0.5vw; font-size: medium;">
+    </span>
+</div>`
+
+});
+
+function start(byButton) {
+    
+    if(byButton==true){
+
+        let minuteInput= document.getElementById("min").value;
+        let secondInput= document.getElementById("sec").value;
+        let valueRank=[parseInt(minuteInput), parseInt(secondInput)];
+        console.log(persOption);
+        console.log(valueRank);
+        showRadomWord();
+        runAll(valueRank, persOption);
+        onTyping();
+        document.getElementById("optionPerso").innerHTML='';
+        
+    }else{
+
+        let valueRank= document.querySelector('select').value;
+        showRadomWord();
+        runAll(valueRank, persOption);
+        onTyping();
+
+    }
+
+}
+
+document.getElementById("start").addEventListener("click", ()=>{
+    start(persOption);
+    initWord();
+})
+
+function stop(result) {
+    //document.body.style.backgroundColor="red";
+    if (result==true) {
+        document.getElementById("menu").style.display="block";
+        document.body.style.backgroundColor=-"red";
+        document.getElementById("welcome-result").innerHTML=`Felicitation ! Vous avez ${mistakes} fautes`;
+    }else{
+        document.body.style.backgroundColor=-"red";
+        document.getElementById("menu").style.display="block";
+        document.getElementById("welcome-result").innerHTML= `Vous avez perdu, tentez encore votre chance`
+    }
+    initWord();
+}
